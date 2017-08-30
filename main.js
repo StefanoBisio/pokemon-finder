385fa44a16c14639ca7484468e59bea7259ee6e7
Stefano Bisio <s.b.milano@gmail.com>
2017-08-30 16:14:39 +0100

    Reduced lines of code

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
-  $('#addPokemon').click(function (e) {
+
+  $('#addPokemon').click(function() {
     var searchTerm = $('#pokemonName').val().toLowerCase();
-    var url = "https://pokeapi.co/api/v2/pokemon/"+ searchTerm;
+    findPokemon(searchTerm);
+  });
+
+  // enables to press enter instead of the clicking the button
+  $('#pokemonName').keypress(function (e) {
+    if (e.which == 13) {
+      event.preventDefault();
+      $('#addPokemon').click();
+    }
+  });
+  $('#random').click(function() {
+    var randomId = Math.floor(Math.random()*803);
+    var url = "https://pokeapi.co/api/v2/pokemon/"+ randomId;
+    findPokemon(randomId);
+
+  });
+
+  function findPokemon(id) {
+    var url = "https://pokeapi.co/api/v2/pokemon/"+ id;
     $.ajax ({
         url: url,
         datatype: "json",
         success: function (e) {
-            console.log(e);
             // Success callback
             $("#hidden").fadeIn();
             $('.pokemonList').append('<li><img src="'+e.sprites.front_default+'"><h2>'+e.name+'!</h2></li>');
@@ -14,31 +32,7 @@ $(document).ready(function(){
         error: function(XMLHttpRequest, textStatus, errorThrown){
                alert("Pokemon not found!");                
         }
-
       });
-  }); // on click
-  $('#pokemonName').keypress(function (e) {
-  if (e.which == 13) {
-    event.preventDefault();
-    $('#addPokemon').click();
-
   }
-
-
-});
-$('#random').click(function () {
-  var randomId = Math.floor(Math.random()*803);
-  var url = "https://pokeapi.co/api/v2/pokemon/"+ randomId;
-  $.ajax ({
-      url: url,
-      datatype: "json",
-      success: function (e) {
-          // Success callback
-          $("#hidden").fadeIn();
-          $('.pokemonList').append('<li><img src="'+e.sprites.front_default+'"><h2>'+e.name+'!</h2></li>');
-          }
-
-    });
-});
 
 }); //ready
